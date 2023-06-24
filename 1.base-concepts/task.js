74bf11f1cf61378f5111a703754b565ce4ebc650
code-turpal <gireevanzor@gmail.com>
2023-06-24 18:38:40 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	let d = b ** 2 - 4 * a * c;
+
+	if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNaN(countMonths)) {
+		let percentMonth = percent / 100 / 12;
+		let bodyCredit = amount - contribution;
+		let feeMonth = bodyCredit * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+		let totalAmount = parseFloat((feeMonth * countMonths).toFixed(2));
+		return totalAmount;
+	}
+
 }
