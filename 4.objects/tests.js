e3d5ad6b823333160a8736cec4ea8e79a2cb9295
code-turpal <gireevanzor@gmail.com>
2023-07-25 19:15:27 +0300

    Четвертое задание обновлено

--- 4.objects/tests.js
+++ 4.objects/tests.js
@@ -1,65 +1,59 @@
-describe("Домашнее задание к занятию 4 «Объекты»", () => {
-  it("Сохранение базовых свойств", () => {
-    let student = new Student("Василиса", "женский", 19);
-    expect(student.name).toEqual("Василиса");
-    expect(student.gender).toEqual("женский");
-    expect(student.age).toEqual(19);
-    expect(student.marks).toEqual([]);
+describe('Домашнее задание к лекции 5 «Классы». Дополнительное задание', () => {
+  describe('Задача №3', () => {
+    let student;
+  
+    beforeEach(function(){
+      student = new Student("Иван Петров");
+    });
+
+    it('создание объекта Student', () => {
+      expect(student).toBeDefined();
+      expect(student.name).toEqual("Иван Петров");
+      expect(student.marks).toEqual({});
+    });
+
+    it('добавление оценок по разным предметам', () => {
+      student.addMark(3, "математика");
+      expect(student.marks).toEqual({"математика": [3]});
+      student.addMark(5, "математика");
+      expect(student.marks).toEqual({"математика": [3, 5]});
+      student.addMark(5, "физика");
+      expect(student.marks).toEqual({"математика": [3, 5], "физика": [5]});
+    });
+
+    it('невозможность добавления некорректных оценок', () => {
+      student.addMark(0, "математика");
+      expect(student.marks).toEqual({});
+      student.addMark(3, "математика");
+      expect(student.marks).toEqual({"математика": [3]});
+      student.addMark(10, "математика");
+      expect(student.marks).toEqual({"математика": [3]});
+      student.addMark(7, "физика");
+      expect(student.marks).toEqual({"математика": [3]});
+    });
+
+    it('подсчёт средней оценки по предмету', () => {
+      student.addMark(3, "математика");
+      student.addMark(5, "математика");
+      expect(student.getAverageBySubject("математика")).toEqual(4);
+    });
+
+    it('подсчёт средней оценки по несуществующему предмету', () => {
+      student.addMark(3, "математика");
+      student.addMark(5, "математика");
+      expect(student.getAverageBySubject("программирование")).toEqual(0);
+    });
+
+    it('подсчёт общей средней оценки пустого объекта оценок', () => {
+      expect(student.getAverage()).toEqual(0);
+    });
+
+    it('подсчёт общей средней оценки', () => {
+      student.addMark(3, "математика");
+      student.addMark(5, "математика");
+      student.addMark(5, "история");
+      student.addMark(5, "история");
+      expect(student.getAverage()).toEqual(4.5);
+    });
   });
-
-  it("Установка предмета работает корректно", () => {
-    let student = new Student("Василиса", "женский", 19);
-
-    student.setSubject("Algebra");
-    expect(student.subject).toEqual("Algebra");
-    
-    student.setSubject("Geometry");
-    expect(student.subject).toEqual("Geometry");
-  });
-
-  it("Добавление группы оценок addMarks работает корректно", () => {
-    let student = new Student("Василиса", "женский", 19);
-    
-    expect(student.marks).toEqual([]);
-
-    student.addMarks(5, 4, 5);
-    expect(student.marks).toEqual([5, 4, 5]);
-
-    student.addMarks(5, 4, 5);
-    expect(student.marks).toEqual([5, 4, 5, 5, 4, 5]);
-  });
-
-  it("Вычисление среднего работает корректно", () => {
-    let student = new Student("Василиса", "женский", 19);
-
-    expect(student.getAverage()).toEqual(0);
-
-    student.addMarks(5, 5, 5);
-    expect(student.getAverage()).toEqual(5);
-
-    student.marks = [3, 4, 5];
-    expect(student.getAverage()).toEqual(4);
-  });
-
-  it("Отчисление студента работает корректно", () => {
-    let student = new Student("Василиса", "женский", 19);
-
-    student.exclude('прогулы');
-    expect(student.excluded).toEqual('прогулы');
-
-    expect(student.subject).toBeUndefined();
-    expect(student.marks).toBeUndefined();
-  });
-
-  it("Невозможность добавлять оценки отчисленному студенту", () => {
-    let student = new Student("Василиса", "женский", 19);
-
-    student.exclude('прогулы');
-    expect(student.excluded).toEqual('прогулы');
-
-    student.addMarks(5, 5, 5);
-    expect(student.marks).toBeUndefined();
-    expect(student.getAverage()).toEqual(0);
-  });
-
 });
